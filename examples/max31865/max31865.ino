f97552192b1942e3db04091cad2aec56f572c942
huanldqm <huanldt@gmail.com>
2024-11-11 16:49:32 +0700

    Update max31865.ino

--- examples/max31865/max31865.ino
+++ examples/max31865/max31865.ino
@@ -31,7 +31,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
 
-  thermo.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  thermo.begin(MAX31865_4WIRE);  // set to 2WIRE or 4WIRE as necessary
 }
 
 
